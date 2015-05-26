db49abf7ef63fcc722b83429705757fb0897df0a
Vinay Gunnam <vinaygunnam@live.com>
2015-05-26 17:21:50 -0500

    Sample entity definition from swagger.

--- index.ts
+++ index.ts
@@ -1,2 +1,17 @@
 var _ = require('lodash');
-var swaggerJson = require('./temp/swagger.json');
+var swaggerJson: SwaggerObject = require('./temp/swagger.json');
+
+var entityName = "User";
+var entityDefinition: Schema = swaggerJson.definitions[entityName];
+
+var toWrite = "interface I" + entityName + " {\n";
+
+if (entityDefinition && entityDefinition.properties) {
+    for (var key in entityDefinition.properties) {
+        toWrite += ("\t" + key + ": any;\n");
+    }
+}
+
+toWrite += "}";
+
+console.log(toWrite);
